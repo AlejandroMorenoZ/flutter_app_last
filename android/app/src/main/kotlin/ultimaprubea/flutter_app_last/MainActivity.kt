f5a4497fe7c9aa01de43527abc21b93143c31a1b
Alejandro Alberto Moreno Zatarain <alalmorenoza@ittepic.edu.mx>
2019-10-07 12:17:41 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/ultimaprubea/flutter_app_last/MainActivity.kt
@@ -0,0 +1,13 @@
+package ultimaprubea.flutter_app_last
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
